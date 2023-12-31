4e2736d67fbbc7d487a6d097a194d22a43a0085a
Rohit Macherla <rm1667@scarletmail.rutgers.edu>
2023-12-30 20:16:36 -0500

    Logging and Exception added

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,8 @@
 import sys
+import logging
 
 def error_message_detail(error, error_details:sys):
-    exc_tb = error_details.exc_info()[2]
+    _,_, exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
     error_message =  "Error occured in python script [{0}] line number [{1}] error message [{2}]".format(
@@ -13,7 +14,7 @@ def error_message_detail(error, error_details:sys):
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.error_message = error_message_detail(error_message, error_detail)
         
     def __str__(self):
         return self.error_message
